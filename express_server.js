92d5353278b0b9f37a163557eced316f1836955d
David Kim <davidjinuk90@gmail.com>
2017-01-22 08:26:30 +0000

    added user specific features so they can only view edit and delete their own content. anyone can still access the short url links

--- express_server.js
+++ express_server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  global: {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  }
 };
+
+let users = {};
 
 function randomString() {
   let result = "";
@@ -27,35 +31,47 @@ function randomString() {
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    username: req.cookies["user_id"],
+    users: users
   };
   res.render("urls_index", templateVars);
 });
 
 //input long url through form
 app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    username: req.cookies["username"]
-  };
-  res.render("urls_new", templateVars);
+  if (users && users[req.cookies.user_id]) {
+    let templateVars = {
+      username: req.cookies["user_id"],
+      users: users,
+      urls: urlDatabase
+    };
+    res.render("urls_new", templateVars);
+  }
 });
 
 //receive form data of long url
 app.post("/urls", (req, res) => {
   let shortURL = randomString();
   let longURL = req.body.longURL;
+  let username = req.cookies["user_id"];
   //add new key-value pair to urlDatabase
-  urlDatabase[shortURL] = longURL;
+  if (urlDatabase[username] === undefined) {
+    urlDatabase[username] = {};
+    urlDatabase[username][shortURL] = longURL;
+  }
+  urlDatabase[username][shortURL] = longURL;
   res.redirect("/urls/" + shortURL);
 });
 
 //redirect short url to long url page
 app.get("/u/:shortURL", (req, res) => {
-  let templateVars = {
-    longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
-  };
-  res.redirect(templateVars.longURL, templateVars);
+  let shortURL = req.params.shortURL;
+  for (let user in urlDatabase) {
+    if (urlDatabase[user][shortURL]) {
+      return res.redirect(urlDatabase[user][shortURL]);
+    }
+  }
+  res.redirect("/urls");
 });
 
 //to show single URL from user input of shortURL to browser
@@ -63,8 +79,10 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    longURL: urlDatabase[req.cookies["user_id"]][req.params.id],
+    username: req.cookies["user_id"],
+    users: users,
+    urls: urlDatabase
   };
   res.render("urls_show", templateVars);
 });
@@ -84,16 +102,74 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls/" + shortURL);
 });
 
+//register page to new page with only form and header
+app.get("/register", (req, res) => {
+  let templateVars = {
+    username: req.cookies["user_id"],
+    users: users,
+    urls: urlDatabase
+  };
+  res.render("urls_register", templateVars);
+});
+
+//receieve form data from registration
+app.post("/register", (req, res) => {
+  let randomID = randomString();
+  let email = req.body.email;
+  let password = req.body.password;
+
+  for (let userID in users) {
+    if (email === users[userID].email) {
+      return res.status(400).send("You are already registered!");
+    }
+  }
+
+  if (email === '' || password === '') {
+    res.status(400).send("Not valid!");
+  } else {
+    users[randomID] = {
+      id: randomID,
+      email: req.body.email,
+      password: req.body.password
+    };
+    res.cookie("user_id", randomID);
+  }
+  res.redirect("/urls");
+});
+
+//login page
+app.get("/login", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["user_id"],
+    users: users,
+  };
+  res.render("urls_login", templateVars)
+});
+
 //receive form data from partial header
 //set cookie called username to info from form
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+  let email = req.body.email;
+  let password = req.body.password;
+
+  for (let userID in users) {
+    if (email !== users[userID].email) {
+      return res.status(403).send("Please Register!");
+    }
+    if ((email === users[userID].email) && (password !== users[userID].password)) {
+      return res.status(403).send("Incorrect Password!");
+    }
+    if ((email === users[userID].email) && (password === users[userID].password)) {
+      res.cookie("user_id", users[userID].id);
+      return res.redirect("/urls");
+    }
+  }
 });
 
 //clears the cookie after pressing logout
 app.post("/logout", (req, res) => {
-  res.cookie("username", '');
+  res.cookie("user_id", '');
   res.redirect("/urls");
 });
 
